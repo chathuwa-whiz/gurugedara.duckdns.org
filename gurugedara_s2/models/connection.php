5cb61670771869fb132c767df6224f93c8d2fd6a
chathuwa-whiz <navodchathushka@gmail.com>
2025-12-10 18:08:55 +0530

    2025-12-10

--- gurugedara_s2/models/connection.php
+++ gurugedara_s2/models/connection.php
@@ -4,7 +4,7 @@ class Connection{
 
 	public function connect(){
 
-		$link = new PDO("mysql:host=127.0.0.1;dbname=gurugedara_db1", "chathuwa", "Chathuwa@2025");
+		$link = new PDO("mysql:host=127.0.0.1;dbname=gurugedara_db2", "chathuwa", "Chathuwa@2025");
 
 		$link -> exec("set names utf8");
 
